d3294dbdf650f38933b8e11a280d1d24a0a04cc4
Yinghao Lei <leiyinghao.bj@gmail.com>
2014-07-17 15:05:04 +0800

    auth  with github

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,11 @@
+class SessionsController < ApplicationController
+  def create
+#    debugger 
+   user  = User.from_auth(request.env['omniauth.auth'])
+   session[:user_id] = user.id
+   flash[:notice] = "welcome  #{user.nickname}"
+   redirect_to  posts_path
+    
+
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Auth::Application.config.secret_key_base = '4055891b6bcc32395ab0b1ff04d99573b79f57585cc5c08942ed9dd767d0435364c7555a93a886b7aff1377b627139a17d10ce6d80e4bc628b52e7769f929aed'
