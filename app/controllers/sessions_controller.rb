d7fdda3c367926814923fc52e3e2a9d8821ee671
Yinghao Lei <leiyinghao.bj@gmail.com>
2014-07-17 15:05:50 +0800

    auth  with github

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,9 @@
 class SessionsController < ApplicationController
   def create
 #    debugger 
-   user  = User.from_auth(request.env['omniauth.auth'])
-   session[:user_id] = user.id
-   flash[:notice] = "welcome  #{user.nickname}"
-   redirect_to  posts_path
-    
-
+    user = User.from_auth(request.env['omniauth.auth'])
+    session[:user_id] = user.id
+    flash[:notice] = "welcome  #{user.nickname}"
+    redirect_to posts_path
   end
 end
